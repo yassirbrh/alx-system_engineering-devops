66e5f4be5e0cf89a0085930aa22122d6840cbb09
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-10-03 01:06:56 +0100

    Puppet manifest to install web server Nginx.

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,28 @@
+# Puppet manifest to install web server Nginx.
+
+exec { 'install_nginx':
+    provider => shell,
+    command  => 'apt-get -y update && apt-get install -y nginx
+    mkdir -p /var/www/helloworld
+    echo "Hello World!" > /var/www/helloworld/index.html
+    echo -e "http {
+        root /var/www/helloworld;
+        index index.html;
+        server {
+                listen 80;
+                add_header X-Served-By $HOSTNAME;
+                location \ {
+                }
+                location /redirect_me {
+                    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+                }
+                error_page 404 /404_redirection;
+                location = /404_redirection {
+                        internal;
+                        return 404 \"Ceci n\'est pas une page\n\n\";
+                }
+        }
+}
+events {
+}" > /etc/nginx/nginx.conf;service nginx start;'
+}
