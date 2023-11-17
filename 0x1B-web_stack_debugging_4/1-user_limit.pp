a8383c20194212894d02384717091348bc6bb638
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-11-17 18:43:58 +0000

    Fix the issue in the task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,11 +2,11 @@
 
 exec {'change-os-configuration-for-holberton-user':
     provider => shell,
-    command  => 'sudo sed -i "s/nofile 4/nofile 4000/" /etc/security/limits.conf'
-    after    => Exec['change-os-configuration-for-holberton-user-2']
+    command  => 'sudo sed -i "s/nofile 5/nofile 5000/" /etc/security/limits.conf'
+    before   => Exec['change-os-configuration-for-holberton-user-2']
 }
 
 exec {'change-os-configuration-for-holberton-user-2':
-  provider => shell,
-  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+    provider => shell,
+    command  => 'sudo sed -i "s/nofile 4/nofile 4000/" /etc/security/limits.conf'
 }
