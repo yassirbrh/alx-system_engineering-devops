505669e278d47be8c70a8a7afbc37369cc58b394
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-11-17 18:25:32 +0000

    Fix the puppet-lint issue

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Puppet manifest to fix the Nginx issue
 
 exec {'fix--for-nginx':
-	provider => shell,
-	command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-	before   => Exec['restart']
+    provider => shell,
+    command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+    before   => Exec['restart']
 }
 
 exec {'restart':
-	provider => shell,
-	command  => 'sudo service nginx restart'
+    provider => shell,
+    command  => 'sudo service nginx restart'
 }
