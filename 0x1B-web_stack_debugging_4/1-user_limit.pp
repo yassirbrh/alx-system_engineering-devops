5334e8e206b18843852baea1a14a841dbc107a16
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-11-17 18:35:01 +0000

    Fix the Puppet-lint issue in the task 1*

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,12 @@
 #Manifest to configure the OS to increase the number of the files opened
 
-exec {'Increase Limits':
-     provider => shell,
-     command  => 'sudo sed -i "s/nofile 4/nofile 4000/" /etc/security/limits.conf && sudo sed -i "s/nofile 5/nofile 5000/" /etc/security/limits.conf'
+exec {'change-os-configuration-for-holberton-user':
+    provider => shell,
+    command  => 'sudo sed -i "s/nofile 4/nofile 4000/" /etc/security/limits.conf'
+    after    => Exec['change-os-configuration-for-holberton-user-2']
+}
+
+exec {'change-os-configuration-for-holberton-user-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
